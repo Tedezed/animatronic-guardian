29e63ed4755b05fa830b7d703bec05e0d30d33e1
Juan Manuel Torres <juanmanuel.torres@aventurabinaria.es>
2020-02-29 13:44:48 +0100

    update

---
+++ Arduino_and_74HC595/Arduino_and_74HC595.ino
@@ -0,0 +1,146 @@
+// Animatronic Guardian
+// By Tedezed
+// Code for motors 28BYJ-48
+#include "Arduino.h"
+#include "ControllSteper.h"
+
+using namespace std;
+
+Motors motors[6];
+int size_motors = 6;
+
+int pins_motor0 [4] = {0, 1, 2, 3};
+int pins_motor1 [4] = {4, 5, 6, 7};
+int pins_motor2 [4] = {8, 9, 10, 11};
+int pins_motor3 [4] = {12, 13, 14, 15};
+int pins_motor4 [4] = {16, 17, 18, 19};
+int pins_motor5 [4] = {20, 21, 22, 23};
+
+int mov_motor = 500; // Steps in a unit
+int delay_motor = 4000;
+bool debug = false;
+
+// Conf 74HC595
+#define SER_Pin 4 //SER_IN
+#define RCLK_Pin 3 //L_CLOCK
+#define SRCLK_Pin 2 //CLOCK
+#define NUM_REGISTERS 2 //Number of circuits 74HC595 in chain
+
+//Initaize shifter using the Shifter library
+Shifter shifter(SER_Pin, RCLK_Pin, SRCLK_Pin, NUM_REGISTERS);
+
+int array_steps [4][4] =
+{
+  {1, 1, 0, 0},
+  {0, 1, 1, 0},
+  {0, 0, 1, 1},
+  {1, 0, 0, 1}
+};
+int size_array_pins = 4;
+
+String option = "";
+void setup()
+{
+  Serial.begin(9600);
+  for (int m = 0; m < size_motors; m++)
+  {
+    motors[m].home = 0;
+    motors[m].position = 0;
+    motors[m].step = 0;
+  }
+  motors[0].pins = pins_motor0;
+  motors[1].pins = pins_motor1;
+  motors[2].pins = pins_motor2;
+  motors[3].pins = pins_motor3;
+  motors[4].pins = pins_motor4;
+  motors[5].pins = pins_motor5;
+}
+
+int only_one = 300;
+// Start Guardian
+void loop(){
+  //motors[0].steps_to = motors[0].steps_to -1000;
+  //motors[1].steps_to = motors[1].steps_to +1100;
+  //exec_steps_multiple_motors(array_steps, size_array_pins, 2, motors, 1700);
+  //delay(3500);
+  
+  if (Serial.available()>0){
+    option=Serial.readString();
+    if(option == "home\n") {
+      Serial.println("Go to home");
+      if (only_one < 200) {
+        motors[only_one].steps_to = motors[only_one].home;
+        exec_steps_multiple_motors(array_steps, size_array_pins, 1, motors, delay_motor, debug);
+      }
+      else 
+      {
+        for (int m = 0; m < size_motors; m++)
+        {
+           motors[m].steps_to = motors[m].home;
+        }
+        exec_steps_multiple_motors(array_steps, size_array_pins, size_motors, motors, delay_motor, debug);
+      }
+    }
+    else if(option == "new-home\n") {
+      Serial.println("New home");
+      for (int m = 0; m < size_motors; m++)
+      {
+         motors[m].home = 0;
+         motors[m].step = 0;
+      }
+    }
+    else if(option == "up\n") {
+      Serial.println("Up");
+      if (only_one < 200) {
+        motors[only_one].steps_to = motors[only_one].step - mov_motor;
+        exec_steps_multiple_motors(array_steps, size_array_pins, 1, motors, delay_motor, debug);
+      }
+      else
+      {
+        for (int m = 0; m < size_motors; m++)
+        {
+           motors[m].steps_to = motors[m].step - mov_motor;
+        }
+        exec_steps_multiple_motors(array_steps, size_array_pins, size_motors, motors, delay_motor, debug);
+      }
+    }
+    else if(option == "down\n") {
+      Serial.println("Down");
+      if (only_one < 200) {
+        motors[only_one].steps_to = motors[only_one].step + mov_motor;
+        exec_steps_multiple_motors(array_steps, size_array_pins, size_motors, motors, delay_motor, debug);
+      }
+      else
+      {
+        for (int m = 0; m < size_motors; m++)
+        {
+           motors[m].steps_to = motors[m].step + mov_motor;
+        }
+        exec_steps_multiple_motors(array_steps, size_array_pins, size_motors, motors, delay_motor, debug);
+      }
+    }
+    else if(option == "only-one\n") {
+      Serial.println("Only-one");
+      bool not_break = true;
+      while (not_break) {
+        if (Serial.available()>0){
+          only_one=Serial.parseInt();
+          Serial.print("Select motor: ");
+          Serial.println(only_one);
+          not_break = false;
+        }
+      }
+    }
+    else if(option == "all\n") {
+      Serial.println("Select all motors");
+      only_one = 300;
+    }
+    else
+    {
+      Serial.print("Option not found: ");
+      Serial.println(option);
+    }
+    Serial.flush(); 
+  }
+  delay(1);
+}
---
+++ Arduino_and_74HC595/ControllSteper.h
@@ -0,0 +1,162 @@
+#include "Arduino.h"
+
+#ifndef CONTROL_STEPER_H
+#define CONTROL_STEPER_H
+
+#include <Shifter.h>
+
+int* array_motors;
+int* current_pins;
+
+struct Motors {
+  int* pins;
+  int home;
+  int position;
+  int step;
+  int steps_to;
+};
+extern Motors motors[6];
+extern Shifter shifter;
+
+void conf_pin_mode(int size_pins,int* input)
+{
+  for (int i = 0; i < size_pins; i++)
+  {
+    pinMode(input[i], OUTPUT);
+    Serial.print("Config pin:");
+    Serial.println(input[i]);
+  }
+}
+
+int to_positive(int input)
+{
+    if (input < 0)
+    {
+      input = input * -1;
+    }
+    return input;
+}
+
+int to_negative(int input)
+{
+    if (input > 0)
+    {
+      input = input * -1;
+    }
+    return input;
+}
+
+int distance(int init_step, int end_step)
+{
+    if (init_step < end_step)
+    {
+      return to_positive(init_step - end_step);
+    }
+    return to_negative(end_step - init_step);
+}
+
+int max_motor_distance(Motors* motors, int size_motors, bool debug=false)
+{
+  int max_distance = 0;
+  for (int m = 0; m < size_motors; m++)
+  {
+    int distance_motor = to_positive(distance(motors[m].step, motors[m].steps_to));
+    if (debug)
+    {
+      Serial.print("Actual step: ");
+      Serial.print(motors[m].step);
+      Serial.print(" Step to: ");
+      Serial.print(motors[m].steps_to);
+      Serial.print(" Distance: ");
+      Serial.println(distance_motor);
+    }
+    if (distance_motor > max_distance)
+    {
+      max_distance = distance_motor;
+    }
+  }
+  return max_distance;
+}
+
+Motors* exec_steps_multiple_motors(
+  int array_steps[][4],
+  int size_array_pins,
+  int size_motors,
+  Motors* motors,
+  int delay_step=2000,
+  bool debug=false
+  )
+{
+  for (int m = 0; m < size_motors; m++)
+  {
+     Serial.println(motors[m].step);
+  }
+  // State 0 = stop
+  // State 1 = positive rotation
+  // State -1 = negative rotation
+  int state_motor = 0;
+  int max_step = max_motor_distance(motors, size_motors, debug);
+  Serial.print("Exec steps: ");
+  Serial.println(max_step);
+  for (int s = 0; s < max_step; s++)
+  {
+    for (int m = 0; m < size_motors; m++)
+    {
+      // Position control
+      int next_position = motors[m].position + 1;
+      if (next_position >= size_array_pins)
+      {
+        next_position = 0;
+        motors[m].position = next_position;
+      }
+      else
+      {
+        motors[m].position = next_position;
+      }
+      
+      for (int i = 0; i < size_array_pins; i++)
+      {
+        // Reverse control
+        int distance_motor = distance(motors[m].step, motors[m].steps_to);
+        if (debug)
+        {
+          Serial.print("Pin: ");
+          Serial.print(motors[m].pins[i]);
+          Serial.print(" Position: ");
+          Serial.print(array_steps[next_position][i]);
+          Serial.print(" Next_position: ");
+          Serial.print(next_position);
+          Serial.print(" Distance: ");
+          Serial.println(distance_motor);
+        }
+        if (distance_motor < 0)
+        {
+          shifter.setPin(motors[m].pins[i], array_steps[next_position][(i-(size_array_pins-1))*-1]);
+          state_motor = -1;
+        }
+        else if (distance_motor == 0)
+        {
+          state_motor = 0;
+        }
+        else
+        {
+          shifter.setPin(motors[m].pins[i], array_steps[next_position][i]);
+          state_motor = 1;
+        }
+      }
+      shifter.write();
+      //Serial.println("-----");
+      motors[m].step = motors[m].step + state_motor;
+      //Serial.println(state_motor);
+      //Serial.println(motors[m].step);
+    }
+    delayMicroseconds(delay_step);
+  }
+  for (int m = 0; m < size_motors; m++)
+  {
+     Serial.println(motors[m].step);
+  }
+  return motors;
+}
+
+#endif
